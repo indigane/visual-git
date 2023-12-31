189055229e1381327d8abd5dc7d9a633eccb5e44
indigane <indigane@users.noreply.github.com>
2023-12-31 22:20:29 +0200

    Change colors, sizes and gaps

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -36,7 +36,9 @@ class Node {
 async function renderCommits(commits) {
   const commitsContainer = document.querySelector('.commits');
   const edgesContainer = document.querySelector('.edges');
-  const colors = ['#dd826f', '#8bacd2', '#bad56a', '#ae7fba', '#e8b765', '#f8ed73', '#bab6d8', '#f0cee5', '#a2d2c7'];
+  //const colors = ['#dd826f', '#8bacd2', '#bad56a', '#ae7fba', '#e8b765', '#f8ed73', '#bab6d8', '#f0cee5', '#a2d2c7'];
+  //const colors = ['#68023F', '#008169', '#EF0096', '#00DCB5', '#FFCFE2', '#003C86', '#9400E6', '#009FFA', '#FF71FD', '#7CFFFA', '#6A0213', '#008607', '#F60239', '#00E307', '#FFDC3D'];
+  const colors = ['#ee6677', '#228833', '#4477aa', '#ccbb44', '#66ccee', '#aa3377', '#bbbbbb'];
 
   // Collect paths of nodes
   const paths = [];
@@ -118,11 +120,12 @@ async function renderCommits(commits) {
     }
     // Node
     const node = nodeForCommitId.get(commit.id);
+    const color = colors[node.path.columnIndex % colors.length];
     commitsContainer.insertAdjacentHTML('beforeend', `
     <div class="commit" style="--row: ${node.row}; --column: ${node.path.columnIndex};" data-id="${node.commit.id}">
-      <div class="graph">
+      <div class="graph" style="color: ${color};">
         <svg>
-          <circle>
+          <circle></circle>
         </svg>
       </div>
       <div class="message">${asTextContent(node.commit.subject)}</div>
@@ -133,30 +136,28 @@ async function renderCommits(commits) {
     const columnWidth = 32;
     const xOffset = columnWidth / 2;
     const yOffset = rowHeight / 2;
+    const endGap = rowHeight / 6;
     for (const [parentIndex, parentId] of commit.parents.entries()) {
       const isPrimaryParent = parentIndex === 0;
       const parentNode = nodeForCommitId.get(parentId);
-      if (node.path === parentNode.path) {
+      const edgeElement = document.createElementNS('http://www.w3.org/2000/svg', 'polyline');
+      const points = [];
+      if (parentNode === undefined) {
+        // TODO: Handle situation where parent commit has not been parsed.
+        // There should be an edge drawn without a destination.
+      }
+      else if (node.path === parentNode.path) {
         // Edge is within the same path. Draw a simple line.
-        const color = colors[node.path.columnIndex % colors.length];
-        const edgeElement = document.createElementNS('http://www.w3.org/2000/svg', 'polyline');
-        edgesContainer.appendChild(edgeElement);
-        const points = [];
         const startX = node.path.columnIndex;
         const startY = node.row;
         points.push(`${startX * columnWidth + xOffset},${startY * rowHeight + yOffset}`);
         const endX = parentNode.path.columnIndex;
         const endY = parentNode.row;
         points.push(`${endX * columnWidth + xOffset},${endY * rowHeight + yOffset}`);
-        edgeElement.setAttribute('points', [points].join(' '));
-        edgeElement.style.stroke = color;
+        edgeElement.style.stroke = colors[node.path.columnIndex % colors.length];
       }
       else if (isPrimaryParent) {
         // Edge is converging. Draw a line with a corner.
-        const color = colors[node.path.columnIndex % colors.length];
-        const edgeElement = document.createElementNS('http://www.w3.org/2000/svg', 'polyline');
-        edgesContainer.appendChild(edgeElement);
-        const points = [];
         const startX = node.path.columnIndex;
         const startY = node.row;
         points.push(`${startX * columnWidth + xOffset},${startY * rowHeight + yOffset}`);
@@ -166,15 +167,10 @@ async function renderCommits(commits) {
         const endX = parentNode.path.columnIndex;
         const endY = parentNode.row;
         points.push(`${endX * columnWidth + xOffset},${endY * rowHeight + yOffset}`);
-        edgeElement.setAttribute('points', [points].join(' '));
-        edgeElement.style.stroke = color;
+        edgeElement.style.stroke = colors[node.path.columnIndex % colors.length];
       }
       else {
         // Edge is diverging. Draw a line with a corner.
-        const color = colors[parentNode.path.columnIndex % colors.length];
-        const edgeElement = document.createElementNS('http://www.w3.org/2000/svg', 'polyline');
-        edgesContainer.appendChild(edgeElement);
-        const points = [];
         const startX = node.path.columnIndex;
         const startY = node.row;
         points.push(`${startX * columnWidth + xOffset},${startY * rowHeight + yOffset}`);
@@ -184,9 +180,13 @@ async function renderCommits(commits) {
         const endX = parentNode.path.columnIndex;
         const endY = parentNode.row;
         points.push(`${endX * columnWidth + xOffset},${endY * rowHeight + yOffset}`);
-        edgeElement.setAttribute('points', [points].join(' '));
-        edgeElement.style.stroke = color;
+        edgeElement.style.stroke = colors[parentNode.path.columnIndex % colors.length];
       }
+      edgesContainer.appendChild(edgeElement);
+      edgeElement.setAttribute('points', [points].join(' '));
+      // Gap
+      const polylineLength = edgeElement.getTotalLength();
+      edgeElement.setAttribute('stroke-dasharray', polylineLength);
     }
   }
 }
